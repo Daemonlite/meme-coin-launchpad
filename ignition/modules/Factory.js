a625deba0baf7738f82091f0d72036b9733e7533
Daemonlite <khemikal2016@gmail.com>
2025-07-17 14:22:49 +0000

    deployment

--- ignition/modules/Factory.js
+++ ignition/modules/Factory.js
@@ -4,6 +4,15 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
-module.exports = buildModule("FactoryModule", (m) => {
+const FEE = ethers.parseUnits("0.01", 18);
 
+module.exports = buildModule("FactoryModule", (m) => {
+    // fetch the contract
+    const fee = m.getParameter("fee", FEE);
+
+    // deploy the contract
+    const factory = m.contract("Factory",[fee])
+
+    //Return the contract
+    return {factory}
 })
